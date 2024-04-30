c969b0b56088f6521e659372529f7019fed8a08f
Jordan Rains <snapjordan@gmail.com>
2024-04-29 19:33:00 -0500

    Co-authored-by: TotallyMoxie <TotallyMoxie@users.noreply.github.com> Co-authored-by: amlambert99 <amlambert99@users.noreply.github.com>

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TeamListComponent } from './team-list/team-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    TeamListComponent,
+
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
