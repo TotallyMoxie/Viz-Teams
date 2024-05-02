8eec6252f905d427f800c61e4b7d3bb02c83211a
Dannie Soest <danniesoest@gmail.com>
2024-05-02 18:59:35 -0500

    add button for teams

--- front-end/src/app/team-list/team-list.component.ts
+++ front-end/src/app/team-list/team-list.component.ts
@@ -3,6 +3,9 @@ import { RouterOutlet } from '@angular/router';
 import {MatCardModule} from '@angular/material/card';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatExpansionModule} from '@angular/material/expansion';
+import {MatButtonModule} from '@angular/material/button';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip'
 
 @Component({
   selector: 'app-team-list',
@@ -11,7 +14,10 @@ import {MatExpansionModule} from '@angular/material/expansion';
     RouterOutlet,
     MatCardModule,
     MatDividerModule,
-    MatExpansionModule
+    MatExpansionModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTooltipModule
   ],
   templateUrl: './team-list.component.html',
   styleUrl: './team-list.component.css'
