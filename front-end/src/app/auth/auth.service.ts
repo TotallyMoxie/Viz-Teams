a1bdcd56c1d27b66c05a0cfa19551bc7069ce168
TotallyMoxie <134233554+TotallyMoxie@users.noreply.github.com>
2024-05-06 20:08:57 -0500

    fix env loading

--- front-end/src/app/auth/auth.service.ts
+++ front-end/src/app/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { User } from './User.model';
-import { environment } from '../../environments/environment.development';
+import { environment } from '../../environments/environment';
 
 
 @Injectable({
--- front-end/src/environments/environment.ts
+++ front-end/src/environments/environment.ts
@@ -1,3 +1,3 @@
 export const environment = {
-  backendUrl: "https://viz-teams-1.onrender.com/"
+  backendUrl: "https://viz-teams-1.onrender.com"
 };
