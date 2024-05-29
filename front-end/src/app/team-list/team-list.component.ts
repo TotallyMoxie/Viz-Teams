7ad46eeab8cf1354199d3d54b8f3aa766c4364b1
TotallyMoxie <134233554+TotallyMoxie@users.noreply.github.com>
2024-05-28 19:05:50 -0500

    made it perdy again!

--- front-end/src/app/team-list/team-list.component.ts
+++ front-end/src/app/team-list/team-list.component.ts
@@ -14,6 +14,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 
+
 import {
   DialogData,
   DialogOverviewExampleDialog,
@@ -46,6 +47,9 @@ import { Team } from '../shared/models/team.model';
 })
 
 export class TeamListComponent {
+clearLocalStorage() {
+throw new Error('Method not implemented.');
+}
   teamSubscription: Subscription;
   message: any;
   teams: any;
